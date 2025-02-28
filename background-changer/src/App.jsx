d35a8e39e9ec14e53b1b23e90186c270c2385eff
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-02-28 23:10:49 +0530

    initial commit: add projects source code

---
+++ background-changer/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive')
+
+  return (
+    <div className='w-full h-screen duration-200' style={{background: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 ps-2'>
+        <div className='flex flex-wrap justify-center gap-3 shoadow-lg bg-white px-3 py-2 rounded-3xl'>
+              <button onClick={() => setColor('red')} className='outline-none px-4 py-2 rounded-full shadow-lg text-black' style={{backgroundColor: 'red'}}>Red</button>
+              <button onClick={() => setColor('green')} className='outline-none px-4 py-2 rounded-full shadow-lg text-black' style={{backgroundColor: 'green'}}>Green</button>
+              <button onClick={() => setColor('blue')} className='outline-none px-4 py-2 rounded-full shadow-lg text-black' style={{backgroundColor: 'blue'}}>Blue</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
